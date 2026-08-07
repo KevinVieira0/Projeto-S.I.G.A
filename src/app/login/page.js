ab091ef1da44791a95c5910da271e011e985cc63
FredBSB <frsantiago018@gmail.com>
2026-08-07 14:40:21 -0300

    fix: correction gitignore

--- src/app/login/page.js
+++
@@ -1,9 +0,0 @@
-import LoginCard from "@/components/login/LoginCard";
-
-export default function LoginPage() {
-  return (
-    <main className="flex min-h-screen items-center justify-center bg-white px-4">
-      <LoginCard />
-    </main>
-  );
-}
