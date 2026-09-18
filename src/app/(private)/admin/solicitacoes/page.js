90fcc64b87faebc8df33d3f64e88c56f9ef9e868
FrSantiago <frsantiago018@gmail.com>
2026-09-18 14:16:55 -0300

    feat: "Solicitacoes" screen started

---
+++ src/app/(private)/admin/solicitacoes/page.js
@@ -0,0 +1,15 @@
+
+
+export const metadata = {
+  title: "Solicitações | Projeto S.I.G.A",
+};
+
+export default function AdminSolicitacoesPage() {
+  return (
+        <div className="p-8">
+          <h1 className="text-2xl font-bold text-gray-900">
+            Solicitações
+          </h1>
+        </div>
+  );
+}
